1a6a78819fc23b0ee700d2d3e43a5a577ae3fa77
Denis Chilyakov <chilyakov@gmail.com>
2022-05-30 16:37:18 +0500

    work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,18 @@ public class PlayerController : MonoBehaviour
     public GridLayout gridLayout;
     private Vector3Int cellPosition;
     private Vector3 startPosition;
-    public float speed = 30f;
-
+    public float speed = 5f;
+    private Rigidbody2D rbody;
     void Start()
     {
         SnapToCell();
+        //startPosition = transform.position;
     }
 
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
     void SnapToCell()
     {
         cellPosition = gridLayout.WorldToCell(transform.position);
@@ -22,10 +27,37 @@ public class PlayerController : MonoBehaviour
         //Debug.Log(cellPosition);
     }
 
+
+    private void MouseMovement()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(mousePos);
+            transform.position = Vector2.MoveTowards(transform.position, mousePos, speed * Time.deltaTime);
+            
+            //rbody.MovePosition(mousePos);
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            
+            //this hit.point should give you the same Vector2 results as worldPoint variable
+            //Debug.Log(hit.point);
+            //buildPoint = hit.point;
+            
+            //now you can check if the ray hit a collider on a certain layer 
+            //if (hit.collider.gameObject.layer == buildSite)
+            //{
+            //        PlaceTower(buildPoint);
+            //}
+        }
+    }
+
     void Update()
     {
-        Moving();
-
+        //Moving();
+        MouseMovement();
     }
 
     void Moving()
@@ -62,16 +94,34 @@ public class PlayerController : MonoBehaviour
         //Debug.Log(cellPosition);
         startPosition = transform.position;
         transform.position = gridLayout.CellToWorld(cellPosition);
-        var target = gridLayout.CellToWorld(cellPosition);
+        Vector3 target = gridLayout.CellToWorld(cellPosition);
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = gridLayout.CellToWorld(cellPosition);
+        //rbody.MovePosition(target);
+
+
 
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("GameObject1 collided with " + col.name);
-        transform.position = startPosition;
+        Debug.LogFormat("Coll name: {0}, coll friction: {1} ", col.name, col.friction);
+        //transform.position = startPosition;
     }
 
+    void FixedUpdate() 
+    {
+        //MouseMovement();
+        //Moving();
+        // Vector2 currentPos = rbody.position;
+        // float horizontalInput = Input.GetAxis("Horizontal");
+        // float verticalInput = Input.GetAxis("Vertical");
+        // Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        // inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        // Vector2 movement = inputVector * speed;
+        // Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        // rbody.MovePosition(newPos);
+
+    }
 
   }
 
