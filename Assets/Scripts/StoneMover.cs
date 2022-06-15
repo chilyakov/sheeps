e2d06b87cdd7ee068cef6f648485ba04b6c48a6d
Denis Chilyakov <chilyakov@gmail.com>
2022-06-15 16:14:47 +0500

    work

--- Assets/Scripts/StoneMover.cs
+++ Assets/Scripts/StoneMover.cs
@@ -7,24 +7,38 @@ public class StoneMover : MonoBehaviour
     [SerializeField]
     private float speed = 0.04f;
     [SerializeField]
-    private Vector3 target;
+    private Vector3 target = new Vector3(.5f, 0, 0);
     [SerializeField]
     private Vector3 start;
-
-
+    [SerializeField]
+    private float rotationSpeed = 100f;
     // Start is called before the first frame update
     // void Start()
     // {
         
     // }
+    float angle, radius = 10;
+    float angleSpeed = 2;
+    float radialSpeed = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        if (transform.position == target) {
-            transform.position = start;
-        }
+        angle = Time.deltaTime * angleSpeed;
+        //radius -= Time.deltaTime * radialSpeed;
+ 
+        Vector3 pos = new Vector3();
+        pos.y = Mathf.Cos(Mathf.Deg2Rad*angle);
+        //float y = radius * Mathf.Sin(Mathf.Deg2Rad*angle);
+        //float z = transform.position.z;
+        //float x = transform.position.x;
+
+        transform.RotateAround(target, Vector3.back, Mathf.Deg2Rad*angle);
+        
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+        //if (transform.position.y < target.y) {
+        //     transform.position = start;
+        // }
     }
 
 }
